fc6cff4bace2db460576ccaf7a8954add1296d80
Mihir Vyas <mihirv@vt.edu>
2025-08-07 19:26:39 -0400

    input handling

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ print("Installing dependencies")
 subprocess.run(["pip", "install", "-r", "requirements.txt"], check=True)
 
 
-
 print("Checking for GPU...")
 try:
     import torch
     if not torch.cuda.is_available():
         print(" No GPU detected, we highly recommend that you go to runtime, and switch the runtime type to either a T4 or L4 before continuing")
+        answer = input ("Warning, make sure you change your runtime to GPU, press enter to continue")
 except ImportError:
     print("Installing torch...")
     subprocess.run(["pip", "install", "torch"])
