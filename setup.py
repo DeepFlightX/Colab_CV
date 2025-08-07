4a6c891541d388051699c315aec2c386fd2f89f6
Mihir Vyas <mihirv@vt.edu>
2025-08-07 17:36:23 -0400

    initial test commit with installing of dependencies and running setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+import sys
+
+print("Cloning repos...")
+os.system("git clone https://github.com/WongKinYiu/yolov7.git")
+
+print("Installing dependencies")
+os.system("pip install -r requirements.txt")
+
+
+print("Checking for GPU...")
+try:
+    import torch
+    if not torch.cuda.is_available():
+        print(" No GPU detected, we highly recommend that you go to runtime, and switch the runtime type to either a T4 or L4 before continuing")
+except ImportError:
+    print("Installing torch...")
+    subprocess.run(["pip", "install", "torch"])
+
+
+
+
